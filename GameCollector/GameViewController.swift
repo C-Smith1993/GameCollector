5c3da7a27672098b4b31987fa291386bdb751955
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-02-22 19:51:42 +0000

    - I created a GameViewController class. - I added all of the GameViewController elements to the GameViewController class so that they can be referenced in code.

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Chris Smith on 22/02/2017.
+//  Copyright © 2017 CDSApps. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: AnyObject) {
+    }
+
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+}
