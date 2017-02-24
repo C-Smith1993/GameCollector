e78c66b43f1e9f632dd80578f96c1ae5a3e6aaa5
C-Smith1993 <chrissmithreds93@hotmail.com>
2017-02-24 13:43:35 +0000

    Added the code for the cameraTapped action.

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -49,6 +49,10 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
 
     @IBAction func cameraTapped(_ sender: AnyObject) {
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     @IBAction func addTapped(_ sender: AnyObject) {
